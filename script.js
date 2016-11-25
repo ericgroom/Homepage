1cc454c364e6c628f0ac31839eb217d44a3ee2af
Eric Groom <bcgroom@gmail.com>
2016-11-25 12:42:01 -0800

    implement search for duckduckgo and subreddits

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById("search").addEventListener("submit", function(e) {
+  e.preventDefault();
+  var search_text = document.getElementById("search-text").value;
+  search(search_text);
+
+  function search(query) {
+    if (query[0] != "/" || query[0] == "!") {
+      var url = "https://duckduckgo.com/?q=" + query;
+    } else {
+      if (query.includes("r/")) {
+        url = reddit(query);
+      }
+    }
+    window.location = url;
+  }
+
+  function reddit(query) {
+    query = query.split(" ");
+    var subreddit = query.shift();
+    query = query.join(" ");
+    if (subreddit.startsWith("/r/")){
+      subreddit = subreddit.slice(3)
+    } else if (subreddit.startsWith("r/")) {
+      subreddit = subreddit.slice(2)
+    }
+    query = joinWithPlus(query);
+    var url = "https://www.reddit.com/r/" + subreddit + "/search?q=" + query + "&restrict_sr=on&sort=relevance&t=all";
+    return url;
+  }
+
+  function joinWithPlus(text) {
+    var list = text.split(" ");
+    list = list.join("+");
+    return list;
+  }
+})
