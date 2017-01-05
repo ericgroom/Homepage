8e41f815a1f6ea2d2a99b062ae9be28c2e29bd1f
Eric Groom <bcgroom@gmail.com>
2017-01-05 14:29:37 -0800

    day 3 updates

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,14 +1,12 @@
-var key = "072f5adb0394cd03296056d94597ab18";
-var samp_url = "https://api.darksky.net/forecast/072f5adb0394cd03296056d94597ab18/37.8267,-122.4233";
-var query_url = "";
+var key = "cfa256a72d1dd578a40187c220292d26";
 /*
  * sample api call
- * https://api.darksky.net/forecast/072f5adb0394cd03296056d94597ab18/37.8267,-122.4233
+ * api.openweathermap.org/data/2.5/weather?lat=35&lon=139
  */
 
 var constructURL = function(position) {
-  return "https://api.darksky.net/forecast/" + key + "/" + position.coords.latitude +
-  "," + position.coords.longitude;
+  return "api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude +
+   "&lon" + position.coords.longitude;
 };
 
 var x = document.getElementById("summary");
@@ -26,9 +24,30 @@ function showPosition(position) {
 
 function getWeather(position) {
   var url = constructURL(position);
-  $.getJSON(url, function(data) {
-    alert(data)
-  })
+  // $.getJSON(url, function(data) {
+  //   alert(data.currently.summary);
+  // })
+  testJqueryGetJSON(url);
 };
 
+function testJqueryGetJSON(url) {
+  $.getJSON(url, function(result){
+        if (result.length == 0){
+            alert("nothing") ;
+        }
+        if (result.length){
+            alert("success") ;
+        }
+    }).fail(function( jqxhr, textStatus, error ) {
+       var err = textStatus + ", " + error;
+       console.log( "Request Failed: " + err )
+    });
+}
+
 getLocation(getWeather);
+
+/*
+ * ran into issues using dark skies, will have to rethink method of getting location
+ * it doesn't feel right to ask for it, especially with VPN fucking things up. will
+ * probably just input zip-code, store it as a cookie, and allow the user to change it. 
+ */
