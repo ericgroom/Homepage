12c3278bcead4dcce98355e937cc598c77fdfc0e
Eric Groom <bcgroom@gmail.com>
2017-01-07 12:15:18 -0800

    Store data in cookie for 10 minutes and use that, else check api.

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,9 +1,10 @@
 var key = "cfa256a72d1dd578a40187c220292d26";
 var zip = 95662;
+var weather = {};
 
 var constructURL = function(zipcode) {
   return "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us" + "&APPID="
-  + key;
+  + key + "&units=imperial";
 };
 
 function testJqueryGetJSON(url) {
@@ -23,17 +24,51 @@ function capitalize(str) {
   var first = str.charAt(0);
   first = first.toUpperCase();
   return first + str.slice(1);
-}
+};
 
-function getWeather(zipcode) {
+function pullWeather(zipcode) {
   var url = constructURL(zipcode);
   console.log(url);
   $.getJSON(url, function(data) {
-    var summary = data.weather[0].description;
-    summary = capitalize(summary);
-    console.log(summary);
-    $("#summary").text(summary);
+    weather.summary = data.weather[0].description;
+    weather.summary = capitalize(weather.summary);
+    console.log("summary: " + weather.summary);
+
+    weather.temperature = data.main.temp;
+    console.log("temperature: " + weather.temperature);
+    displayWeather();
+    storeWeather();
   });
-}
+};
+
+function displayWeather() {
+  $("#summary").text(weather.summary + ' | ' + weather.temperature + "°F");
+};
+
+function storeWeather() {
+  var inTenMinutes = new Date(new Date().getTime() + 10 * 60 * 1000);
+  Cookies.set('weather', weather, {
+    expires: inTenMinutes
+  });
+};
+
+function getWeather() {
+  if (Cookies.get('weather') != undefined) {
+    console.log("From cookie");
+    weather = Cookies.getJSON('weather');
+    console.log("summary: " + weather.summary);
+    console.log("temperature: " + weather.temperature);
+    displayWeather();
+  } else {
+    console.log("From api");
+    pullWeather(zip);
+  }
+};
 
 $(getWeather(zip));
+
+$('#summary').click(function() {
+  $('#summary').text('Refreshing...');
+  Cookies.remove('weather');
+  getWeather();
+})
